0b0f48f473e4e489d3d5218dfcbc4f2f920890e5
Shiva gupta <shivaguptaj26@gmail.com>
2025-11-10 22:44:08 +0530

    Time: 69 ms (96.13%) | Memory: 44.3 MB (82.35%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,35 +2,31 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> adj(n);
-        for (int i = 0; i < times.size(); i++) {
-            int u = times[i][0]-1;
-            int v = times[i][1]-1;
-            int w = times[i][2];
-            adj[u].push_back({w,v});
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int>distance(n,1e9);
-        distance[k-1]=0;
-        pq.push({0,k-1});
-        while(!pq.empty()){
-            auto fullNode=pq.top();
+        for (auto &t : times)
+            adj[t[0]-1].push_back({t[1]-1, t[2]});
+        
+        vector<int> dist(n, INT_MAX);
+        dist[k-1] = 0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0, k-1});
+
+        while (!pq.empty()) {
+            auto [dis, node] = pq.top();
             pq.pop();
-            int node=fullNode.second;
-            int weight=fullNode.first;
-            for(auto it : adj[node]){
-                int weight=it.first;
-                int neighbour=it.second;
-                if(distance[node]+weight<distance[neighbour]){
-                    distance[neighbour]=distance[node]+weight;
-                    pq.push({distance[neighbour],neighbour});
+            if (dis > dist[node]) continue;
+            for (auto [neigh, wt] : adj[node]) {
+                if (dis + wt < dist[neigh]) {
+                    dist[neigh] = dis + wt;
+                    pq.push({dist[neigh], neigh});
                 }
             }
         }
-        int ans=0;
-        for(auto num : distance){
-            if(num==1e9) return -1;
-            if(ans<num) ans=num;
+
+        int ans = 0;
+        for (int d : dist) {
+            if (d == INT_MAX) return -1;
+            ans = max(ans, d);
         }
-    return ans;
+        return ans;
     }
 };
