bbb0bad5a0c7af89b2cd5a414afd06c3d8e6a31f
Shiva gupta <shivaguptaj26@gmail.com>
2025-11-10 22:13:45 +0530

    Time: 13 ms (68.83%) | Memory: 16.4 MB (96.14%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            matrix[u][v] = w;
+            matrix[v][u] = w;
+        }
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.size(); j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = 1e9;
+                }
+                if (i == j)
+                    matrix[i][j] = 0;
+            }
+        }
+        vector<int> reach(n, 0);
+        for (int via = 0; via < matrix.size(); via++) {
+            for (int i = 0; i < matrix.size(); i++) {
+                for (int j = 0; j < matrix.size(); j++) {
+                    matrix[i][j] =
+                        min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+                }
+            }
+        }
+        int total = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.size(); j++) {
+                if (matrix[i][j] <= distanceThreshold && i != j) {
+                    reach[i]++;
+                }
+                if (matrix[i][j] == 1e9) {
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+        int mini = INT_MAX;
+        int minCity = -1;
+        for (int i = 0; i < reach.size(); i++) {
+            if (mini >= reach[i]) {
+                mini = reach[i];
+                minCity = i;
+            }
+        }
+        return minCity;
+    }
+};
