a358971689177fdb53de53579881a1fb1dfc501e
Shiva gupta <shivaguptaj26@gmail.com>
2025-11-12 19:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (27.70%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -34,11 +34,3 @@ public:
 
     bool empty() { return (input.size() == 0); };
 };
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
