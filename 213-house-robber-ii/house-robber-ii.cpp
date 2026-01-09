6c06d5d2f3cbca2a8f993b4650f1283ea79360aa
Shiva gupta <shivaguptaj26@gmail.com>
2026-01-09 21:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (7.34%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,34 +1,33 @@
 class Solution {
-public: 
-    int helper(vector<int>&nums,int index,vector<int>&dp){
-        if(index==0){
-            return nums[index];
+public:
+    int helper(vector<int>& money) {
+        vector<int> dp(money.size(), -1);
+        dp[0] = money[0];
+        int neg = 0;
+        for (int i = 1; i < money.size(); i++) {
+            int pick = money[i];
+            if (i > 1)
+                pick += dp[i - 2];
+            int notpick = 0 + dp[i - 1];
+            dp[i] = max(pick, notpick);
         }
-        
-        if(index<0){
-            return 0;
-        }
-
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        int take=nums[index]+helper(nums,index-2,dp);
-        int nottake=0+helper(nums,index-1,dp);
-        return dp[index]=max(take,nottake);
+        return dp[money.size()-1];
     }
-    int rob(vector<int>& money) {
-        vector<int>arr1;
-        vector<int>arr2;
-        vector<int>dp1(money.size(),-1);
-        vector<int>dp2(money.size(),-1);
-        if(money.size()==1) return money[0];
 
-        for(int i=0;i<money.size();i++){
-            if(i!=0)arr1.push_back(money[i]);
-            if(i!=money.size()-1)arr2.push_back(money[i]);
-        }
-        int ans1=helper(arr1,arr1.size()-1,dp1);
-        int ans2=helper(arr2,arr2.size()-1,dp2);
-        return max(ans1,ans2);
+    int rob(vector<int> & money) {
+            vector<int> arr1;
+            vector<int> arr2;
+            if (money.size() == 1)
+                return money[0];
+
+            for (int i = 0; i < money.size(); i++) {
+                if (i != 0)
+                    arr1.push_back(money[i]);
+                if (i != money.size() - 1)
+                    arr2.push_back(money[i]);
+            }
+            int ans1 = helper(arr1);
+            int ans2 = helper(arr2);
+            return max(ans1, ans2);
     }
 };
