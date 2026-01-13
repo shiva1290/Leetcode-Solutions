c40fb020b77c42cdd83968c20bd6322f1482f460
Shiva gupta <shivaguptaj26@gmail.com>
2026-01-13 23:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (84.78%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,int m,int n){
-        if(row<0 || row>=m) return false;
-        if(col<0 || col>=n) return false;
-        return true;
-    }
-
-    int totalWays(int row,int col,int m,int n, vector<vector<int>> &dp){
-        if(row==m-1 && col==n-1){
-            return 1;
-        }
-        int ways1=0;
-        int ways2=0;
-        
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-
-        if(isSafe(row+1,col,m,n)){
-        ways1=totalWays(row+1,col,m,n,dp);
-        }
-        if(isSafe(row,col+1,m,n)){
-        ways2=totalWays(row,col+1,m,n,dp);
-        }
-        return dp[row][col]=ways1+ways2;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return totalWays(0,0,m,n,dp);
+        vector<int> prev(n, 0);
+
+        for (int i = 0; i < m; i++) {
+            vector<int> cur(n, 0);
+            for (int j = 0; j < n; j++) {
+                int right = 0;
+                int down = 0;
+                if (i == 0 && j == 0)
+                    cur[j] = 1;
+                else {
+                    if (i > 0) right = prev[j];
+                    if (j > 0) down = cur[j - 1];
+                    cur[j]=right+down;
+                }
+            }
+            prev = cur;
+        }
+
+        return prev[n-1];
     }
 };
