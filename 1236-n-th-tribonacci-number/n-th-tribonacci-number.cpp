22838f9764b1ccfc953aad653e47a24e0fa0600a
Shiva gupta <shivaguptaj26@gmail.com>
2026-01-31 18:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (37.09%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int helper(int n,unordered_map<int,int>&dp){
-        if(dp.find(n)!=dp.end()){
-            return dp[n];
-        }
-        if(n==0){
-            return 0;
-        } 
-        if(n==1 || n==2){
-            return 1;
-        }
-
-        int result = helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
-        dp[n]=result;
-        return result;
-    }
     int tribonacci(int n) {
-        unordered_map<int,int>dp;
-        return helper(n,dp);
+        vector<int>dp(n+1,0);
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];   
+        }
+        return dp[n];
     }
 };
