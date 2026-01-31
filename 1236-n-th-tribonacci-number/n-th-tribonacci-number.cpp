7be61f7d222c240650d9321edc7f2219d2504fee
Shiva gupta <shivaguptaj26@gmail.com>
2026-01-31 18:43:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.33%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int>dp(n+1,0);
         if(n==0) return 0;
         if(n==1 || n==2) return 1;
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int first=0;
+        int second=1;
+        int third=1;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];   
+            int ans=first+second+third;   
+            first=second;
+            second=third;
+            third=ans;
         }
-        return dp[n];
+        return third;
     }
 };
