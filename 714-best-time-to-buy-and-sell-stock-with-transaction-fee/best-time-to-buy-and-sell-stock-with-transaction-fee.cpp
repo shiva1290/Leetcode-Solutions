d9a804b73754567cb12f4d3dd601c14a5cb955f5
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-03 11:25:49 +0530

    Time: 135 ms (5.21%) | Memory: 109.4 MB (5.79%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int maxProfit(vector<int> arr, int fee) {
-        int n = arr.size();
-        vector<int> curr(2, 0);
-        vector<int> prev(2, 0);
-
-        for (int index = n - 1; index >= 0; index--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (!buy) {
-                    curr[buy] = max((-arr[index] + prev[1]), (0 + prev[0]));
-                }
-                if (buy) {
-                    curr[buy] =
-                        max((arr[index] - fee + prev[0]), (0 + prev[1]));
-                }
-                prev = curr;
-            }
+    int helper(int index, vector<int>&prices, int fee, bool bought,vector<vector<int>>&dp){
+        if(index>=prices.size()){
+            return 0;
         }
-        return curr[0];
+        int sellBought=INT_MIN;
+        int continueBuy=INT_MIN;
+        int buyStock=INT_MIN;
+        int notBuy=INT_MIN;
+        if(dp[index][bought]!=-1){
+            return dp[index][bought];
+        }
+        if(bought){
+            sellBought=prices[index]-fee+helper(index+1,prices,fee,0,dp);
+            continueBuy=helper(index+1,prices,fee,1,dp);
+        }
+        if(!bought){
+            buyStock=-prices[index]+helper(index+1,prices,fee,1,dp);
+            notBuy=helper(index+1,prices,fee,0,dp);
+        }
+        return dp[index][bought]=max(sellBought,max(continueBuy,max(buyStock,notBuy)));
+   }
+    int maxProfit(vector<int>& prices, int fee) {
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return helper (0,prices,fee,0,dp);
     }
 };
