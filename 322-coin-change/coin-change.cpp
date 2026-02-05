8272763311c3b0dc1bb67bea4b22bc4f433692ab
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-05 23:32:49 +0530

    Time: 26 ms (62.37%) | Memory: 44.2 MB (44.09%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, 1e5));
-        for(int coin=0;coin<coins.size();coin++){
-            dp[coin][0] = 0;
-        }
+        vector<int> dp (amount + 1, 1e5);
         for(int am=0;am<=amount;am++){
             if(am%coins[0]==0)
-            dp[0][am] = am / coins[0] ;
+            dp[am] = am / coins[0] ;
         }
         for (int index = 1; index < coins.size(); index++) {
+            vector<int>curr(amount+1,1e5);
+            curr[0]=0;
             for (int am = 1; am <= amount; am++) {
                 int takeAgain = INT_MAX;
-                if (am - coins[index] >= 0 && coins[index]!=1e5) {
-                    takeAgain = 1 + dp[index][am - coins[index]];
+                if (am - coins[index] >= 0) {
+                    takeAgain = 1 + curr[am - coins[index]];
                 }
-                int notTake = dp[index - 1][am];
-                dp[index][am] = min(takeAgain, notTake);
+                int notTake = dp[am];
+                curr[am] = min(takeAgain, notTake);
             }
+            dp=curr;
         }
 
-        return dp[coins.size() - 1][amount] == 1e5
+        return dp[amount] == 1e5
                    ? -1
-                   : dp[coins.size() - 1][amount];
+                   : dp[amount];
     }
 };
