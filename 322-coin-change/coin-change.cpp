0a2d278b623b202b7206920e5cfa905bd5f3e375
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-05 23:27:34 +0530

    Time: 53 ms (20.08%) | Memory: 48.8 MB (39.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,27 @@
 class Solution {
-   public:
-    int helper(int index, vector<int>& coins, int amount,vector<vector<int>>&dp) {
-        if (amount == 0) {
-            return 0;
-        }
-        if (index >= coins.size()) {
-            return 1e9;
-        }
-		if(dp[index][amount]!=-1)
-		{
-			return dp[index][amount];
-		}
-        int takeAgain = INT_MAX;
-        if (amount - coins[index] >= 0) {
-            takeAgain = 1 + helper(index, coins, amount - coins[index],dp);
-        }
-        int notTake = helper(index + 1, coins, amount,dp);
-        return dp[index][amount]=min(takeAgain, notTake);
-    }
+public:
     int coinChange(vector<int>& coins, int amount) {
-		vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-		int ans= (helper(0, coins, amount,dp));
-        return ans==1e9 ? -1 : ans;
+        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, 1e5));
+        for(int coin=0;coin<coins.size();coin++){
+            dp[coin][0] = 0;
+        }
+        for(int am=0;am<=amount;am++){
+            if(am%coins[0]==0)
+            dp[0][am] = am / coins[0] ;
+        }
+        for (int index = 1; index < coins.size(); index++) {
+            for (int am = 1; am <= amount; am++) {
+                int takeAgain = INT_MAX;
+                if (am - coins[index] >= 0 && coins[index]!=1e5) {
+                    takeAgain = 1 + dp[index][am - coins[index]];
+                }
+                int notTake = dp[index - 1][am];
+                dp[index][am] = min(takeAgain, notTake);
+            }
+        }
+
+        return dp[coins.size() - 1][amount] == 1e5
+                   ? -1
+                   : dp[coins.size() - 1][amount];
     }
 };
