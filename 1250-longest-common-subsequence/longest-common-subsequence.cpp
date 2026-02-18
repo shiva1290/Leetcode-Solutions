17a25e3b158247c744647aa6ce8f3221b39a9d09
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-18 21:02:36 +0530

    Time: 27 ms (57.98%) | Memory: 27.3 MB (77.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
-    // int helper(int index1,int index2, string & text1 , string & text2,
-    // vector<vector<int>>& dp){
-    //     if(index1>=n || index2>=m){
-    //         return 0;
-    //     }
-    //     if(text1[index1]==text2[index2]){
-    //         return 1+helper(index1+1,index2+1,text1,text2,dp);
-    //     }
-    //     if(dp[index1][index2]!=-1){
-    //         return dp[index1][index2];
-    //     }
-    //     int take1=helper(index1,index2+1,text1,text2,dp);
-    //     int take2=helper(index1+1,index2,text1,text2,dp);
-    //     return dp[index1][index2]=max(take1,take2);
-    // }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        dp[n - 1][m - 1] = text1[n - 1] == text2[m - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            int equals=text1[i] == text2[m - 1];
-            dp[i][m - 1] = max  (dp[i + 1][m - 1], equals);
-        }
-        for (int j = m - 2; j >= 0; j--) {
-            int equals= text1[n - 1] == text2[j];
-            dp[n - 1][j] = max((dp[n - 1][j + 1]),equals);
-        }
-
-        for (int index1 = n - 2; index1 >= 0; index1--) {
-            for (int index2 = m - 2; index2 >= 0; index2--) {
-                int ans = INT_MIN;
-                if (text1[index1] == text2[index2])
-                    ans = 1 + dp[index1 + 1][index2 + 1];
-                int take1 = dp[index1 + 1][index2];
-                int take2 = dp[index1][index2 + 1];
-                dp[index1][index2] = max(take1, max(take2, ans));
+    int longestCommonSubsequence(string str1, string str2) {
+        vector<vector<int>> dp(str1.size() + 1,
+                               vector<int>(str2.size() + 1, 0));
+        for (int ind1 = str1.size() - 1; ind1 >= 0; ind1--) {
+            for (int ind2 = str2.size() - 1; ind2 >= 0; ind2--) {
+                if (str1[ind1] == str2[ind2]) {
+                    dp[ind1][ind2] = 1 + dp[ind1 + 1][ind2 + 1];
+                } else {
+                    dp[ind1][ind2] =
+                        max(dp[ind1 + 1][ind2], dp[ind1][ind2 + 1]);
+                }
             }
         }
         return dp[0][0];
