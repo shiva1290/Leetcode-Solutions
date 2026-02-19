30fe8563a458589892ceb5544eb4e12853555213
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-19 20:42:28 +0530

    Time: 22 ms (76.37%) | Memory: 26 MB (75.20%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
 int prime=1e9+7;
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
-        for (int i = 0; i <= s.size(); i++) {
-            dp[i][t.size()] = 1;
-        }
+        vector<int>dp (t.size() + 1, 0);
+        dp[t.size()]=1;
         for (int index1 = s.size() - 1; index1 >= 0; index1--) {
+            vector<int>curr (t.size() + 1, 0);
+            curr[t.size()]=1;
             for (int index2 = t.size() - 1; index2 >= 0; index2--) {
                 if (s[index1] == t[index2]) {
-                    int takeBoth = dp[index1 + 1][index2 + 1];
-                    int move1 = dp[index1 + 1][index2];
-                    dp[index1][index2] = (takeBoth + move1)%prime;
+                    int takeBoth = dp[index2 + 1];
+                    int move1 = dp[index2];
+                    curr[index2] = (takeBoth + move1)%prime;
                 } else
-                    dp[index1][index2] = dp[index1 + 1][index2];
+                    curr[index2] = dp[index2];
             }
+            dp=curr;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
