7fc8ae68d7fa090c33239ae71135576a6a7239f3
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-19 21:40:28 +0530

    Time: 131 ms (12.08%) | Memory: 55.1 MB (6.03%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool helper(int index1, int index2, string& str, string& pat,vector<vector<int>>&dp) {
+    bool helper(int index1, int index2, string& str, string& pat,
+                vector<vector<int>>& dp) {
         if (index1 == str.size()) {
-
             while (index2 < pat.size()) {
                 if (pat[index2] != '*')
                     return false;
@@ -13,21 +13,23 @@ public:
         if (index2 >= pat.size()) {
             return index1 == str.size();
         }
-        if(dp[index1][index2]!=-1){
+        if (dp[index1][index2] != -1) {
             return dp[index1][index2];
         }
-        if (str[index1] != pat[index2] && pat[index2] != '*' &&
-            pat[index2] != '?') {
-            return false;
-        } else if (pat[index2] == '?') {
-            return dp[index1][index2]=helper(index1 + 1, index2 + 1, str, pat,dp);
-        } else if (pat[index2] == '*') {
-            return dp[index1][index2]=(helper(index1, index2 + 1, str, pat,dp) ||
-                   helper(index1+1,index2, str, pat,dp));
+        if (str[index1] == pat[index2] || (pat[index2] == '?')) {
+            return dp[index1][index2] =
+                       helper(index1 + 1, index2 + 1, str, pat, dp);
         }
-        return helper(index1 + 1, index2 + 1, str, pat,dp);
+
+        else if (pat[index2] == '*') {
+            return dp[index1][index2] =
+                       (helper(index1, index2 + 1, str, pat, dp) ||
+                        helper(index1 + 1, index2, str, pat, dp));
+        }
+        return false;
     }
-    bool isMatch(string s, string p) { 
-        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
-        return helper(0, 0, s, p,dp); }
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(p.size() + 1, -1));
+        return helper(0, 0, s, p, dp);
+    }
 };
