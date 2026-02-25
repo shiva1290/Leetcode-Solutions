434d1b74de2edda845456c7c7ebf2b57e0e58baa
Shiva gupta <shivaguptaj26@gmail.com>
2026-02-25 21:07:44 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (79.09%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i = s.size() - 1;
-        string temp;
-        string result;
-        while (i >= 0) {
-            if (s[i] == ' ' && temp.empty()) {
-                i--;
-                continue;
-            }
+          int n = s.size();
+        int write = 0;
+        int read = 0;
 
-            else if (s[i] != ' ') {
-                temp += s[i];
-            }
+        while (read < n && s[read] == ' ') read++;
 
-            else if (s[i] == ' ' && !temp.empty()) {
-                reverse(temp.begin(), temp.end());
-                result += temp;
-                result += ' ';
-                temp.clear();
+        while (read < n) {
+            if (s[read] != ' ') {
+                s[write++] = s[read++];
+            } else {
+                s[write++] = ' ';
+                while (read < n && s[read] == ' ') read++;
             }
-            i--;
         }
-        if (!temp.empty()) {
-            reverse(temp.begin(), temp.end());
-            result += temp;
-            result += ' ';
+
+        if (write > 0 && s[write - 1] == ' ') write--;
+
+        s.resize(write);
+
+        reverse(s.begin(), s.end());
+
+        int k = 0;
+        bool wordFound = false;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (!wordFound) {
+                wordFound = true;
+                k = i;
+
+            } else if (wordFound && s[i] == ' ') {
+                reverse(s.begin() + k, s.begin() + i);
+                wordFound = false;
+            }
         }
-        result.pop_back();
-        return result;
+        if (wordFound) {
+            reverse(s.begin() + k, s.end());
+        }
+
+        return s;
     }
 };
