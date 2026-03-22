b4432e2262e42960bcf80e440d0fe2c0e6edea7c
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-22 19:43:48 +0530

    Time: 10 ms (39.07%) | Memory: 22.8 MB (77.66%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    long long hours(vector<int>&piles,int mid){
+    long long bsa(vector<int>&nums,int k){
         long long ans=0;
-        for(int i=0;i<piles.size();i++){
-            ans+=ceil(piles[i]/static_cast<double>(mid));
+        for(auto num : nums){
+            ans+=(num+k-1)/k;
         }
         return ans;
     }
-
-    int binarySearch(int low, int high, vector<int>& piles, int h) {
-        while (low <= high) {
-            long long mid =1LL* low + (high - low) / 2;
-            long long ans = hours(piles, mid);
-            if (ans > h) {
+    int helper(int low,vector<int>&nums,int h,int high){
+        while(low<=high){
+            long long mid=1LL*low+(high-low)/2;
+            long long ans=bsa(nums,mid);
+            if(ans>h){
                 low=mid+1;
-            } else if (ans <= h) {
-                high=mid-1;
             }
+            else high=mid-1;
         }
         return low;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi = INT_MIN;
-        for (auto num : piles) {
-            maxi = max(maxi, num);
+        int maxi=INT_MIN;
+        for(auto p : piles){
+            maxi=max(p,maxi);
         }
-        return binarySearch(1, maxi, piles, h);
+        return helper(1,piles,h,maxi);
     }
 };
