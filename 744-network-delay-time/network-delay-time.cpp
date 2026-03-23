2b3d5e93539300b41701dc7dc0bc6b64a0afa288
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-23 21:02:49 +0530

    Time: 75 ms (85.64%) | Memory: 44.4 MB (53.35%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n);
-        for (auto &t : times)
-            adj[t[0]-1].push_back({t[1]-1, t[2]});
-        
-        vector<int> dist(n, INT_MAX);
-        dist[k-1] = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0, k-1});
+    int dijkstra(vector<vector<int>>& times, int n, int k) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        pq.push({0, k - 1});
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < times.size(); i++) {
+            int parent = times[i][0] - 1;
+            int neigh = times[i][1] - 1;
+            int weight = times[i][2];
+            adj[parent].push_back({neigh, weight});
+        }
 
+        vector<int> distance(n, 1e9);
+        distance[k - 1] = 0;
         while (!pq.empty()) {
-            auto [dis, node] = pq.top();
+            auto p = pq.top();
             pq.pop();
-            if (dis > dist[node]) continue;
-            for (auto [neigh, wt] : adj[node]) {
-                if (dis + wt < dist[neigh]) {
-                    dist[neigh] = dis + wt;
-                    pq.push({dist[neigh], neigh});
+            int node = p.second;
+            int dist = p.first;
+            for (auto it : adj[node]) {
+                int distanceHere = it.second;
+                if (dist + distanceHere < distance[it.first]) {
+                    distance[it.first] = dist + distanceHere;
+                    pq.push({distance[it.first], it.first});
                 }
             }
         }
-
         int ans = 0;
-        for (int d : dist) {
-            if (d == INT_MAX) return -1;
-            ans = max(ans, d);
+        for (int i = 0; i < n; i++) {
+            if (distance[i] == 1e9)
+                return -1;
+            ans = max(ans, distance[i]);
         }
         return ans;
+    }
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        return dijkstra(times, n, k);
     }
 };
