1b99ff444c8f502a95b1cbcda34afda2febdb8f4
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-23 19:57:43 +0530

    Time: 190 ms (13.64%) | Memory: 111.1 MB (20.80%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& arr) {
-        int n = arr.size();
-        int target = 0;
-        if (arr.size() == 1)
+    bool helper(int index,vector<int>&nums,int sum,vector<vector<int>>&dp){
+        if(index==nums.size()){
+            return sum==0;
+        }
+        if(sum==0){
+            return true;
+        }
+        if(sum<0){
             return false;
-        for (int i = 0; i < arr.size(); i++) {
-            target += arr[i];
         }
-        if (target % 2 != 0)
-            return false;
-        vector<bool> prev((target / 2) + 1, false);
-
-        prev[0] = true;
-
-        if (arr[0] <= target / 2) {
-            prev[arr[0]] = true;
+        if(dp[index][sum]!=-1){
+            return dp[index][sum]==1;
         }
-        for (int index = 1; index < n; index++) {
-            vector<bool> curr((target / 2) + 1, false);
-            for (int k = 1; k <= target / 2; k++) {
-
-                if (arr[index] <= k) {
-                    curr[k] = prev[k] || prev[k - arr[index]];
-                } else {
-                    curr[k] = prev[k];
-                }
-            }
-            prev = curr;
+        int take=helper(index+1,nums,sum-nums[index],dp);
+        int notTake=helper(index+1,nums,sum,dp);
+        return dp[index][sum]= take | notTake;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(auto num : nums){
+            sum+=num;
         }
-        return prev[target / 2];
+        if(sum%2!=0) return false;
+        else sum=sum/2;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(sum+1,-1));
+        return helper(0,nums,sum,dp);
     }
 };
