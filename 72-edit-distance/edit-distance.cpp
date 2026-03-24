9f893b6a97810a742185b109b0163defe8c73bfa
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-24 22:09:16 +0530

    Time: 5 ms (73.75%) | Memory: 13.1 MB (73.37%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    // int helper(int i, int j, string& word1, string& word2,
-    //            vector<vector<int>>& dp) {
-    //     if (i >= word1.size() || j >= word2.size()) {
-    //         if (j < word2.size())
-    //             return word2.size() - j;
-    //         if (i < word1.size())
-    //             return word1.size() - i;
-    //         return 0;
-    //     }
-    //     // if(dp[i][j]!=-1){
-    //     //     return dp[i][j];
-    //     // }
-    //     if (word1[i] == word2[j]) {
-    //         return helper(i + 1, j + 1, word1, word2, dp);
-    //     }
-    //     int ins = 1 + helper(i, j + 1, word1, word2, dp);
-    //     int del = 1 + helper(i + 1, j, word1, word2, dp);
-    //     int rep = 1 + helper(i + 1, j + 1, word1, word2, dp);
-    //     return dp[i][j] = min(ins, min(del, rep));
-    // }
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for (int i = n; i >= 0; i--) {
-           dp[i][m] = n - i;
-        }
-        for (int j = m; j >= 0; j--) {
-            dp[n][j] = m - j;
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (word1[i] == word2[j]) {
-                    dp[i][j] = dp[i + 1][j + 1];
-                    continue;
-                }
-                int ins = dp[i][j + 1];
-                int del = dp[i + 1][j];
-                int rep = dp[i + 1][j + 1];
-                dp[i][j] = 1 + min(ins, min(del, rep));
+    int helper(int index1, int index2, string& word1, string& word2,vector<vector<int>>&dp){
+        if(index1>=word1.size() || index2>=word2.size()){
+            if(index1>=word1.size() && index2<word2.size()){
+                return word2.size()-index2;
             }
+            if(index2>=word2.size() && index1<word1.size()){
+                return word1.size()-index1;
+            }
+            else return 0;
         }
-        return dp[0][0];
+        if(dp[index1][index2]!=-1){
+            return dp[index1][index2];
+        }
+        if(word1[index1]==word2[index2]){
+            return helper(index1+1,index2+1,word1,word2,dp);
+        }
+        int ins=1+helper(index1,index2+1,word1,word2,dp);
+        int del=1+helper(index1+1,index2,word1,word2,dp);
+        int rep=1+helper(index1+1,index2+1,word1,word2,dp);
+        return dp[index1][index2]=min(ins,min(del,rep));
+    }
+    int minDistance(string word1, string word2) {
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
+        return helper(0,0,word1,word2,dp);
     }
 };
