3d900e59f26367e1f705c4513b130aeea52c847d
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-24 21:25:41 +0530

    Time: 664 ms (5.06%) | Memory: 296.5 MB (11.79%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int lowerBound(int number, vector<int>& arr) {
-        int low = 0;
-        int high = arr.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] >= number) {
-                high = mid - 1;
-            } else
-                low = mid + 1;
+    int helper(int index,int prev, vector<int>&nums,vector<vector<int>>&dp){
+        if(index==nums.size()) return 0;
+        if(dp[index][prev+1]!=-1){
+            return dp[index][prev+1];
         }
-        return low;
+        int take=INT_MIN;
+        int notTake;
+        if(prev==-1 || nums[prev]<nums[index]) take=1+helper(index+1,index,nums,dp);
+        notTake=helper(index+1,prev,nums,dp);
+        return dp[index][prev+1]=max(take,notTake);
     }
-
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> lenArray;
-        lenArray.push_back(nums[0]);
-        for (int i = 1; i < nums.size(); i++) {
-            if (lenArray.back() < nums[i]) {
-                lenArray.push_back(nums[i]);
-            } else {
-                int index = lowerBound(nums[i], lenArray);
-                lenArray[index] = nums[i];
-            }
-        }
-        return lenArray.size();
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        int ans=helper(0,-1,nums,dp);
+        return ans;
     }
 };
