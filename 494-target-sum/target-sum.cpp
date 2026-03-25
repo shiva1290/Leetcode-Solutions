54e9998c8272cdcba71e1aa2ac27ec0c38c00480
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-25 21:29:35 +0530

    Time: 944 ms (6.75%) | Memory: 11.6 MB (84.07%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-
-    int helper(int index, vector<int>& nums, int currentSum, int target,
-               int totalSum, vector<vector<int>>& dp) {
-        if (index == nums.size()) {
-            return currentSum == target;
+    int helper(int index,vector<int>&nums,int target){
+        if(index==nums.size()){
+            if(target==0) return 1;
+            return 0;
         }
-
-        int dpIndex = currentSum + totalSum;
-
-        if (dp[index][dpIndex] != -1) {
-            return dp[index][dpIndex];
-        }
-
-        int add = helper(index + 1, nums, currentSum + nums[index], target,
-                         totalSum, dp);
-
-        int sub = helper(index + 1, nums, currentSum - nums[index], target,
-                         totalSum, dp);
-
-        return dp[index][dpIndex] = (add + sub);
+        int add=helper(index+1,nums,target+nums[index]);
+        int subtract=helper(index+1,nums,target-nums[index]);
+        return add+subtract;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = 0;
-        for (int num : nums)
-            totalSum += num;
-        int n=nums.size();
-        vector<vector<int>> dp(n, vector<int>(2 * totalSum + 1, -1));
-
-        return helper(0, nums, 0, target, totalSum, dp);
+        return helper(0,nums,target);
     }
 };
