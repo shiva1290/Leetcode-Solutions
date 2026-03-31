23baffef8e18bc3a09b19e69739bf4c109b6e063
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-31 21:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (50.67%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    // int helper(int r, int c, int n, int m, vector<vector<int>>& dp) {
-    //     if (r == n - 1 && c == m - 1) {
-    //         return 1;
-    //     }
-    //     if (r > n - 1 || c > m - 1) {
-    //         return 0;
-    //     }
-    //     if (dp[r][c] != -1) {
-    //         return dp[r][c];
-    //     }
-    //     int down = 0;
-    //     int right = 0;
-    //     if (r + 1 < n)
-    //         down = helper(r + 1, c, n, m, dp);
-    //     if (c + 1 < m)
-    //         right = helper(r, c + 1, n, m, dp);
-    //     return dp[r][c] = down + right;
-    // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for(int i=0;i<m;i++){
-            dp[i][n-1]=1;
-        }
+    vector<vector<int>>dp(m,vector<int>(n,0));
         for(int i=0;i<n;i++){
             dp[m-1][i]=1;
         }
-        for (int r = m - 2; r >= 0; r--) {
-            for (int c = n - 2; c >= 0; c--) {
-                int down = 0;
-                int right = 0;
-                if (r + 1 < m)
-                    down = dp[r + 1][c];
-                if (c + 1 < n)
-                    right = dp[r][c + 1];
-                dp[r][c] = down + right;
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
+                int right=dp[j][i+1];
+                int down=dp[j+1][i];
+                dp[j][i]=right+down;
             }
         }
         return dp[0][0];
