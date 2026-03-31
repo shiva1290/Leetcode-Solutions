708e3ab006545e25ff593cbc0e05301423992c3c
Shiva gupta <shivaguptaj26@gmail.com>
2026-03-31 22:52:31 +0530

    Time: 27 ms (39.12%) | Memory: 85.8 MB (33.12%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
-    vector<int>nextSmaller(vector<int>& heights){   
+    vector<int>pse(vector<int>&heights){
         int n=heights.size();
-        stack<int>st;
         vector<int>ans(n,-1);
+        stack<int>st;
+        st.push(0);
+        for(int i=1;i<heights.size();i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(!st.empty()) ans[i]=st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int>nse(vector<int>&heights){
+        int n=heights.size();
+        vector<int>ans(n,n);
+        stack<int>st;
         st.push(n-1);
         for(int i=n-2;i>=0;i--){
             while(!st.empty() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            if(!st.empty()){
-                ans[i]=st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int>prevSmaller(vector<int>& heights){
-        int n=heights.size();
-        stack<int>st;
-        vector<int>ans(n,-1);
-        st.push(0);
-        for(int i=1;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                ans[i]=st.top();
-            }
+            if(!st.empty()) ans[i]=st.top();
             st.push(i);
         }
         return ans;
     }
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>nst=nextSmaller(heights);
-        vector<int>pst=prevSmaller(heights);
+        vector<int>prev=pse(heights);
+        vector<int>next=nse(heights);
+        // for(auto num : prev){
+        //     cout<<num<<" ";
+        // }
+        // cout<<"\n";
+        // for(auto num : next){
+        //     cout<<num<<" ";
+        // }
         int maxArea=0;
         for(int i=0;i<heights.size();i++){
-            int prevIndex=pst[i];
-            int nextIndex=nst[i];
-            if(nextIndex==-1)nextIndex=n;
-            int area=heights[i]*(nextIndex-prevIndex-1);
-            maxArea=max(maxArea,area);
+            maxArea=max(maxArea,(heights[i]*(next[i]-prev[i]-1)));
         }
         return maxArea;
     }
