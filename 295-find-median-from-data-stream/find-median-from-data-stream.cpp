d01f88dd9dc0ae4764280991da7c7781b39fc0ef
Shiva gupta <shivaguptaj26@gmail.com>
2026-04-06 16:12:21 +0530

    Time: 80 ms (50.98%) | Memory: 150.8 MB (7.15%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,38 +1,32 @@
 class MedianFinder {
 public:
-    priority_queue<double> maxPq;
-    priority_queue<double, vector<double>, greater<double>> minPq;
-    int size = 0;
-    MedianFinder() {}
-
-    void addNum(int num) {
-        size++;
-        if (maxPq.empty()) {
-            maxPq.push(num);
-            return;
-        }
-        if (!maxPq.empty()) {
-            if (num <= maxPq.top()) {
-                maxPq.push(num);
-            }
-            else minPq.push(num);
-        }
-
-        if (!maxPq.empty() && maxPq.size() > minPq.size()+1) {
-            minPq.push(maxPq.top());
-            maxPq.pop();
-        }
-        if(!minPq.empty() && minPq.size()>maxPq.size()){
-            maxPq.push(minPq.top());
-            minPq.pop();
-        }
+priority_queue<double> maxPq;
+priority_queue<double,vector<double>,greater<double>> minPq;
+int size=0;
+    MedianFinder() {
     }
 
+    void addNum(int num) {
+        if(maxPq.size()==0 || maxPq.top()>num){
+        maxPq.push(num);
+        }
+        else minPq.push(num);
+        if(!maxPq.empty() && maxPq.size()>minPq.size()+1){
+            double n=maxPq.top();
+            maxPq.pop();
+            minPq.push(n);
+        }
+        if(!minPq.empty() && minPq.size()>maxPq.size()){
+            double n=minPq.top();
+            minPq.pop();
+            maxPq.push(n);
+        }
+        size++;
+    }
+    
     double findMedian() {
-        if (size % 2 == 0) {
-            return (maxPq.top() + minPq.top()) / 2.0;
-        } else
-            return maxPq.top();
+        if(size%2==0) return (minPq.top()+maxPq.top())/2.0;
+        else return maxPq.top();
     }
 };
 
