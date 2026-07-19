f5b08ec484f88a46971d3997ba5bb32fb5b6187a
Shiva gupta <shivaguptaj26@gmail.com>
2026-07-19 14:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (6.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-     bool isMatched(char open,char close){
-        if (
-        (open=='(' && close==')' ) || 
-        (open=='{' && close=='}') ||
-        (open=='[' && close==']'))
-        {
-        return true;
-    }
-        else return false;
-    }
-    bool isValid(string str) {
-    stack<char> st;
-    for(int i=0;i<str.size();i++){
-        if(str[i]=='(' ||  str[i]=='{' || str[i]=='['){
-            st.push(str[i]);
-        }
-        else {
-            if (st.empty()) return false;
-
-            char ch=st.top();
-            st.pop();
-            
-            if(!isMatched(ch,str[i])){
-                return false;
+    bool isValid(string s) {
+        stack<int> st;
+        for (auto n : s) {
+            if (n == '{' || n == '[' || n == '(')
+                st.push(n);
+            else if ((n == '}' && (!st.empty() && st.top() == '{')) ||
+                     (n == ']' && (!st.empty() && st.top() == '[')) ||
+                     (n == ')' && (!st.empty() &&  st.top() == '('))) {
+                st.pop();
             }
+            else return false;
         }
-    }
-    return st.empty();
+        return st.empty();
     }
 };
