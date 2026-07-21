79ee152b6e5e5c9edfbfcb70939fe265d026906f
Shiva gupta <shivaguptaj26@gmail.com>
2026-07-21 13:09:57 +0530

    Time: 116 ms (5.03%) | Memory: 23.1 MB (56.27%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,28 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (!root) {
-            return 0;
-        }
-        int left = height(root->left);
-        if (left == -1)
-            return -1;
-
-        int right = height(root->right);
-        if (right == -1)
-            return -1;
-
-        if (abs(left - right) > 1)
-            return -1;
-
-        return 1 + max(left, right);
+    int DFS(TreeNode* root,bool &ans){
+        if(!root) return -1;
+        int left=1+DFS(root->left,ans);
+        int right=1+DFS(root->right,ans);
+        cout<<root->val<<" "<<left<<" "<<right<<"\n";
+        if(abs(left-right)>1) ans=false;
+        return max(left,right);
     }
-    bool isBalanced(TreeNode* root) { return height(root) != -1; }
+    bool isBalanced(TreeNode* root) {
+        bool ans=true;
+        DFS(root,ans);
+        return ans;
+    }
 };
