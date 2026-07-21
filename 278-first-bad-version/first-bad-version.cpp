c32f8dcb8ef31d95535ac290dfab710962b9dda7
Shiva gupta <shivaguptaj26@gmail.com>
2026-07-21 13:23:22 +0530

    Time: 2 ms (57.69%) | Memory: 7.9 MB (73.39%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,15 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=low + (high - low) / 2;
-            if(isBadVersion(mid)){
-                high=mid-1;
-            }
-            else low=mid+1;
+      int low=0;
+      int high=n;
+      while(low<=high){
+        int mid=low+(high-low)/2;
+        if(isBadVersion(mid)){
+            high=mid-1;
         }
-        return high+1;
+        else low=mid+1;
+      }
+      return low;
     }
 };
