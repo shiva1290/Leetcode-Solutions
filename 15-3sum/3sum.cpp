38ef190c4bc81b0b7784bdd44dc2ce8c19b2bd2b
Shiva gupta <shivaguptaj26@gmail.com>
2026-07-22 17:49:36 +0530

    Time: 143 ms (11.18%) | Memory: 33 MB (14.21%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        set<vector<int>> st;
-        vector<vector<int>> ans;
-        for (int k = 0; k < nums.size(); k++) {
-            if (k>0 && nums[k] == nums[k - 1])
-                continue;
-            int i = k+1;
-            int j = nums.size() - 1;
-            while (i < j) {
-                int sum = nums[j] + nums[i] + nums[k];
-                if (sum == 0) {
-                    st.insert({nums[i], nums[j], nums[k]});
-                    i++;
-                    j--;
-                    while (i<nums.size() && nums[i] == nums[i - 1])
-                        i++;
-                    while (j>=0 && nums[j] == nums[j + 1])
-                        j--;
-                } else if (sum < 0) {
-                    i++;
-                } else
-                    j--;
+        sort(nums.begin(),nums.end());
+        set<tuple<int,int,int>> st;
+      for(int i=0;i<nums.size();i++){
+        int j=i+1;
+        int k=nums.size()-1;
+        while(j<=k && j!=k){
+            if(nums[i]+nums[j]+nums[k]==0){
+                st.insert({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
             }
+            else if(nums[i]+nums[j]+nums[k]>0){
+                k--;
+            }
+            else j++;
         }
-        for (auto it : st) {
-            ans.push_back(it);
-        }
-        return ans;
+      }
+      vector<vector<int>>ans;
+      for(auto [first,second,third] : st){
+        ans.push_back({first,second,third});
+      }
+      return ans;
     }
 };
